cbfc1a0a4be79aa242a297ee72fde52e352b90d9
Jorgecas71 <jorgecas71@gmail.com>
2024-07-02 12:08:34 +0200

    added test

--- src/main/java/dev/jorge/Person.java
+++ src/main/java/dev/jorge/Person.java
@@ -1,5 +1,41 @@
 package dev.jorge;
 
 public class Person {
+
+    private String name;
+    private String lastName;
+    private String documentNumber;
+    private int yearOfBirth;
+
+    public Person(String name, String lastName, String documentNumber, int yearOfBirth) {
+        this.name = name;
+        this.lastName = lastName;
+        this.documentNumber = documentNumber;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public void setDocumentNumber(String DocumentNumber) {
+        this.documentNumber = DocumentNumber;
+    }
+
+    public int yearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
     
 }
--- src/test/java/dev/jorge/PersonTest.java
+++ src/test/java/dev/jorge/PersonTest.java
@@ -1,5 +1,38 @@
 package dev.jorge;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class PersonTest {
-    
+    @Test
+    void testPersonHasAttributes() {
+        Person pepa = new Person("Pepa", "Flores", null, 0);
+        assertEquals(4, pepa.getClass().getDeclaredFields().length);
+    }
+
+    @Test
+    void testGetName() {
+        Person pepa = new Person("Pepa", "Flores", null, 0);
+        assertEquals("Pepa", pepa.getName());
+   
+    }
+
+    @Test
+    void testGetLastName() {
+        Person pepa = new Person("Pepa", "Flores", null, 0);
+        assertEquals("Flores", pepa.getLastName());
+    }
+
+    @Test
+    public void testGetDocumentNumber() {
+        Person pepa = new Person("Pepa", "Flores", "12345678", 1980);
+        assertEquals("12345678", pepa.getDocumentNumber());
+    }
+
+    @Test
+    public void testYearOfBirth() {
+        Person pepa = new Person("Pepa", "Flores", "12345678", 1980);
+        assertEquals(1980, pepa.yearOfBirth());
+    }
 }
